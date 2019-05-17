3647de891d0599a9856e54332f9f1bcd08440edc
Okx <okx@okx.sh>
2019-05-17 16:44:30 +0100

    do uuid comparasion

--- src/main/java/sh/okx/metahider/MetaHider.java
+++ src/main/java/sh/okx/metahider/MetaHider.java
@@ -132,7 +132,7 @@ public class MetaHider extends JavaPlugin {
           StructureModifier<List<WrappedWatchableObject>> modifier = packet.getWatchableCollectionModifier();
           List<WrappedWatchableObject> read = modifier.read(0);
 
-          if (player == entity
+          if (player.getUniqueId().equals(entity.getUniqueId())
               || !(entity instanceof LivingEntity)
               || entity instanceof EnderDragon
               || entity instanceof Wither
