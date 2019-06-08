cdab02e989b48cb3e320ec66521e55cc3fbd399d
Okx <okx@okx.sh>
2019-06-08 21:41:16 +0100

    hide horse stats

--- src/main/java/sh/okx/metahider/MetaHider.java
+++ src/main/java/sh/okx/metahider/MetaHider.java
@@ -7,25 +7,46 @@ import com.comphenix.protocol.events.PacketAdapter;
 import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.events.PacketEvent;
 import com.comphenix.protocol.reflect.StructureModifier;
+import com.comphenix.protocol.wrappers.WrappedAttribute;
+import com.comphenix.protocol.wrappers.WrappedAttributeModifier;
 import com.comphenix.protocol.wrappers.WrappedWatchableObject;
+import org.bukkit.Bukkit;
 import org.bukkit.Color;
 import org.bukkit.Material;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeInstance;
+import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.EnderDragon;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.Horse;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Llama;
+import org.bukkit.entity.Pig;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Vehicle;
 import org.bukkit.entity.Wither;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.LeatherArmorMeta;
 import org.bukkit.inventory.meta.PotionMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionData;
+import org.spigotmc.event.entity.EntityDismountEvent;
+import org.spigotmc.event.entity.EntityMountEvent;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
-public class MetaHider extends JavaPlugin {
+public class MetaHider extends JavaPlugin implements Listener {
   @Override
   public void onEnable() {
     saveDefaultConfig();
@@ -132,8 +153,8 @@ public class MetaHider extends JavaPlugin {
           StructureModifier<List<WrappedWatchableObject>> modifier = packet.getWatchableCollectionModifier();
           List<WrappedWatchableObject> read = modifier.read(0);
 
-          if (player.getUniqueId().equals(entity.getUniqueId())
-              || !(entity instanceof LivingEntity)
+          if (!(entity instanceof LivingEntity)
+              || player.getUniqueId().equals(entity.getUniqueId())
               || entity instanceof EnderDragon
               || entity instanceof Wither
               || entity.getPassengers().contains(player)) {
@@ -150,7 +171,101 @@ public class MetaHider extends JavaPlugin {
           }
         }
       });
+      getServer().getPluginManager().registerEvents(this, this);
     }
+    if (getConfig().getBoolean("hide-rideable-info")) {
+      manager.addPacketListener(new PacketAdapter(this, PacketType.Play.Server.UPDATE_ATTRIBUTES) {
+        @Override
+        public void onPacketSending(PacketEvent event) {
+          PacketContainer packet = event.getPacket();
+          Player player = event.getPlayer();
+          if (player.hasPermission("metahider.bypass")) {
+            return;
+          }
+
+          // only applicable to entities that are rideable (minecarts, horses, etc)
+          Entity entity = packet.getEntityModifier(event.getPlayer().getWorld()).read(0);
+          if (!(entity instanceof Vehicle) || entity.getPassengers().contains(player)) {
+            return;
+          }
+
+          List<WrappedAttribute> oldAttributes = packet.getAttributeCollectionModifier().read(0);
+          List<WrappedAttribute> attributes = new ArrayList<>(oldAttributes.size());
+          for (WrappedAttribute attribute : oldAttributes) {
+            String key = attribute.getAttributeKey();
+            // remove movement speed and jump strength from attributes
+            if (key.equals("generic.movementSpeed") || key.equals("horse.jumpStrength")) {
+              attribute = WrappedAttribute.newBuilder(attribute).baseValue(0).build();
+            }
+            attributes.add(attribute);
+          }
+          packet.getAttributeCollectionModifier().write(0, attributes);
+        }
+      });
+    }
+  }
+
+  @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
+  public void onMount(EntityMountEvent e) {
+    updateMount(e.getEntity(), e.getMount());
+  }
+
+  @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
+  public void onMount(EntityDismountEvent e) {
+    updateMount(e.getEntity(), e.getDismounted());
+  }
+
+  private void updateMount(Entity entity, Entity mount) {
+    if (!(entity instanceof Player) || !(mount instanceof LivingEntity)) {
+      return;
+    }
+    LivingEntity livingMount = (LivingEntity) mount;
+
+    Bukkit.getScheduler().runTask(this, () -> {
+      double health = livingMount.getHealth();
+      // force update of the health of the mount
+      // todo only send the packet to the player
+      livingMount.setHealth(1);
+      livingMount.setHealth(health);
+
+      // tell the client the real movement and jump
+      PacketContainer handle = new PacketContainer(PacketType.Play.Server.UPDATE_ATTRIBUTES);
+      handle.getIntegers().write(0, livingMount.getEntityId());
+      List<WrappedAttribute> attributes = new ArrayList<>(2);
+      attributes.add(wrapAttribute(handle, livingMount.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED), "generic.movementSpeed"));
+      if (livingMount instanceof Horse) {
+        attributes.add(wrapAttribute(handle, livingMount.getAttribute(Attribute.HORSE_JUMP_STRENGTH), "horse.jumpStrength"));
+      }
+      System.out.println(attributes);
+      handle.getAttributeCollectionModifier().write(0, attributes);
+
+      try {
+        ProtocolLibrary.getProtocolManager().sendServerPacket((Player) entity, handle);
+      } catch (InvocationTargetException e) {
+        e.printStackTrace();
+      }
+    });
+  }
+
+  /**
+   * Turns a Bukkit AttributeInstance into a ProtocolLib WrappedAttribute
+   */
+  private WrappedAttribute wrapAttribute(PacketContainer packet, AttributeInstance instance, String name) {
+    Collection<AttributeModifier> oldModifiers = instance.getModifiers();
+    Collection<WrappedAttributeModifier> modifiers = new HashSet<>(2);
+    for (AttributeModifier modifier : oldModifiers) {
+      modifiers.add(WrappedAttributeModifier.newBuilder(modifier.getUniqueId())
+          .name(modifier.getName()).amount(modifier.getAmount())
+          .operation(WrappedAttributeModifier.Operation.fromId(modifier.getOperation().ordinal()))
+          .build());
+    }
+
+    return WrappedAttribute.newBuilder()
+        .baseValue(instance.getBaseValue())
+        .attributeKey(name)
+        .modifiers(modifiers)
+        .packet(packet)
+        .build();
   }
 
   private boolean shouldBeObfuscated(Material type) {
